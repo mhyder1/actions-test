f498ddebe87d2889678c06e4c15e46765241e17f
unknown <hyder1@gmail.com>
2021-10-21 15:28:04 -0400

    jobs

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const octokit = new Octokit();
 
 async function getBuildStats() {
     try {
-        let { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
+        let { data: [ jobs ] } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
             owner: 'mhyder1',
             repo: 'actions-test',
             run_id: process.env.runId
           })
-        console.log(JSON.stringify(data, null, 2))
+        // console.log(JSON.stringify(data, null, 2))
+        console.log({jobs})
         // let { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/jobs/{job_id}', {
         //     owner: 'mhyder1',
         //     repo: 'actions-test',
