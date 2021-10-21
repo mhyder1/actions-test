02a88706b1466c1c7f7988b2baeabf08387369f3
unknown <hyder1@gmail.com>
2021-10-21 13:54:22 -0400

    github

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const { Octokit } = require("@octokit/core");
 // const octokit = require('@octokit/rest')()
-const octokit = new Octokit({ auth: `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` });
-//{ auth: `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` }
+const octokit = new Octokit();
 
 async function getBuildStats() {
     try {
-        let { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/jobs/{job_id}', {
+        let test = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/jobs', {
             owner: 'mhyder1',
             repo: 'actions-test',
-            job_id: 'run-actions',
-            // workflow_file_name: 'CI.yml',
-        })
+            run_id: 'Run cowsay package'
+          })
+        console.log(test)
+        // let { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/jobs/{job_id}', {
+        //     owner: 'mhyder1',
+        //     repo: 'actions-test',
+        //     job_id: 'run-actions',
+        //     // workflow_file_name: 'CI.yml',
+        // })
         console.log(data)
     } catch(error) {
         console.log(error)
