cacec7f91e88f5217ca10723ca70e463cfb9be65
unknown <hyder1@gmail.com>
2021-10-21 15:36:26 -0400

    jobs

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ async function getBuildStats() {
             run_id: process.env.runId
           })
         // console.log(JSON.stringify(data, null, 2))
-        console.log({jobs})
-        // let { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/jobs/{job_id}', {
-        //     owner: 'mhyder1',
-        //     repo: 'actions-test',
-        //     job_id: 'run-actions',
-        //     // workflow_file_name: 'CI.yml',
-        // })
-        // console.log(data)
+        const job_id = jobs[0].id
+        let { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/jobs/{job_id}', {
+            owner: 'mhyder1',
+            repo: 'actions-test',
+            job_id,
+        })
+        console.log({data})
     } catch(error) {
         console.log(error)
     }
